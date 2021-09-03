e04af15d37886c96f98b2c43fe942d60d0bc4e1e
Norton Rayan Meira de Oliveira <norton_rayan@hotmail.com>
2021-09-02 21:50:33 -0300

    iniciando Rotas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Home } from './src/screens/Home';
 import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
 import { Rajdhani_500Medium, Rajdhani_700Bold } from "@expo-google-fonts/rajdhani";
 import AppLoading from "expo-app-loading";
 import { StatusBar } from 'expo-status-bar';
-import { Background } from './src/components/Background'
+import { Routes } from './src/routes';
+import { Background } from './src/components/Background';
 
 export default function App() {
 
@@ -23,7 +23,7 @@ export default function App() {
   return (
     <Background>
       <StatusBar style={"light"} translucent />
-      <Home />
+      <Routes />
     </Background>
   )
 }
--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-
 import { Home } from "../screens/Home";
 import { SignIn } from "../screens/SignIn";
+
+const { Navigator, Screen } = createStackNavigator();
+
+export function AuthRoutes() {
+    return (
+        <Navigator
+            defaultScreenOptions={{ headerShown: false }}
+            screenOptions={{
+                presentation: 'modal',
+                cardStyle: { backgroundColor: 'transparent' }
+            }}>
+            <Screen name="SignIn" component={SignIn} />
+            <Screen name="Home" component={Home} />
+        </Navigator>
+    )
+}
